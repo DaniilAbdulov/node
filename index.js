5d5cdfa5acbd5ab1fa8a8399f13611d6801f0732
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-20 21:40:27 +0500

    CodeWars

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-console.log('Hello Node.js');
+function solve(compasses, gears, tablets) {
+    function sevenPoints(c,g,t){
+        if(c === g && g === t){
+            return c * 7
+        }else{
+            let one = c;
+            let two = g;
+            let three = t;
+            let count = 0;
+            while(one != 0 && two != 0 && three!=0){
+                one -= 1;
+                two -= 1;
+                three -=1;
+                count += 7;
+            }
+            return count;
+        }
+    }
+    let seven = sevenPoints(compasses,gears,tablets);
+    function squares(c,g,t){
+        return c**2 + g**2 + t**2
+    }
+    let square = squares(compasses, gears, tablets);
+    return seven + square
 
-const myName = 'Daniil';
-
-console.log(myName)
+  }
+console.log( solve(2,1,1) )
