8ff434b9aa8688e46329a2b0e00515d32b4e6e98
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-29 18:43:16 +0500

    modified:   08-http/index.js

--- 08-http/index.js
+++ 08-http/index.js
@@ -1,16 +1,33 @@
 const http = require("http");
 const PORT = 5000;
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") {
-        res.writeHead(204, { "Content-Type": "image/x-icon" });
-        res.end();
-        return;
-    }
+const comments = [
+    { id: 100, text: "First", author: "Daniil" },
+    { id: 101, text: "Second", author: "Kamila" },
+];
 
-    console.log(req);
-    res.statusCode = 200;
-    res.end("Greetings from the http server");
+const server = http.createServer((req, res) => {
+    if (req.url === "/html") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/html");
+        res.write("<html><body><div>");
+        res.write("<h1>Greetings from the http server</h1>");
+        res.write("</div></body></html>");
+        return res.end();
+    }
+    if (req.url === "/text") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "text/plain");
+        return res.end("This is plain text");
+    }
+    if (req.url === "/json") {
+        res.statusCode = 200;
+        res.setHeader("Content-type", "application/json");
+        return res.end(JSON.stringify(comments));
+    }
+    res.statusCode = 404;
+    res.setHeader("Content-type", "text/html");
+    return res.end("<h1>Page not found</h1>");
 });
 
 server.listen(PORT, () => {
