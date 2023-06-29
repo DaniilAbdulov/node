778933f36fdcc08ce96f71aaa9cea4051ffb7e59
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-29 06:21:28 +0500

    html

--- 08-http/index.mjs
+++ 08-http/index.mjs
@@ -1,11 +1,11 @@
 import http from "http";
-const hostname = "127.0.0.1";
 const port = 3000;
 const server = http.createServer((req, res) => {
+    console.log(req);
     res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Hello World</h1>");
 });
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at ${port}`);
 });
