716b6d26062a9a8da3705fb05b9e279bcae1bce7
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-06-30 21:22:25 +0500

    promises

--- 05-fs/promises.js
+++ 05-fs/promises.js
@@ -1,9 +1,29 @@
+const { writeFileSync, readFileSync } = require("fs");
 const fs = require("fs/promises");
+const path = require("path");
 
-fs.writeFile("./first.txt", "First file text")
+const dataText = "Hello. My name is Daniil";
+const secondLine = "And i would like to be frontend developer";
+
+fs.writeFile(path.resolve(__dirname, "./first.txt"), dataText)
     .then(() => console.log("File was written"))
-    .then(() => fs.appendFile("./first.txt", "\nOne more line"))
+    .then(() =>
+        fs.appendFile(path.resolve(__dirname, "./first.txt"), `\n${secondLine}`)
+    )
     .then(() => console.log("Appended"))
-    .then(() => fs.rename("./first.txt", "./05-fs/new-name.txt"))
-    .then(() => console.log("file was rename"))
     .catch((err) => console.log(err));
+
+// async function writeAndCount() {
+//     try {
+//         await fs.writeFile("./first.txt", dataText); // записывает первую строку
+//         console.log("File was written");
+//         await fs.appendFile("./first.txt", `\n${secondLine}`); // добавляет вторую строку
+//         console.log("Appended");
+//         const length = (await fs.readFile("./first.txt")).toString().length; // читает оба файла и узнает их длину
+//         await fs.writeFile("textLengt.txt", length.toString()); // записывает длину в новый файл await
+//         fs.unlink("./first.txt"); // удаляет первый файл
+//         console.log("File was deleted");
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
---
+++ 05-fs/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+const { writeFileSync, readFileSync } = require("fs");
+// const fs = require("fs/promises");
+// const path = require("path");
+
+// const dataText = "Hello. My name is Daniil";
+// const secondLine = "And i would like to be frontend developer";
