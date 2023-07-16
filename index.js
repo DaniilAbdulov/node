465b6281e3f86cf9a014b9f2156af12b26e3bd24
DaniilAbdulov <abdulov.dany@yandex.ru>
2023-07-16 17:54:36 +0500

    Codewars

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 //for tasks from LeetCode and CodeWars
-function rotate(str) {
-    //code here
+function evenLast(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        if (i % 2 == 0) {
+            sum += numbers[i] * numbers[numbers.length - 1];
+        }
+    }
+    return sum;
 }
+console.log(evenLast([2, 3, 4, 5]));
