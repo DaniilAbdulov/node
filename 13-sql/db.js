0eb96b28bc2a89777cd83a2a66bc86a77a525078
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-07-18 20:49:38 +0500

    Well Done

--- 13-sql/db.js
+++ 13-sql/db.js
@@ -3,10 +3,10 @@ const { password } = require("pg/lib/defaults");
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "1234",
+    password: "0896",
     host: "localhost",
     port: 5432,
-    database: "node_postgres",
+    database: "postgres",
 });
 
 module.exports = pool;
