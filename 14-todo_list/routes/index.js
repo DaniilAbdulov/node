405b07fc38881564ce9f3694777046122b8bc2d2
Daniil Abdulov <118374108+DaniilAbdulov@users.noreply.github.com>
2023-07-24 06:34:40 +0500

    Delete 14-todo_list directory

--- 14-todo_list/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getHomePage } = require("../controllers/getHomePage");
-const tasksRouter = require("./tasks");
-
-router.get("/", getHomePage);
-router.use("/tasks", tasksRouter);
-
-module.exports = router;
--- 14-todo_list/routes/tasks.js
+++
@@ -1,9 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const taskController = require("../controllers/tasks");
-
-router.get("/", taskController.getTasks);
-router.post("/", taskController.createTask);
-router.delete("/:tasksId", taskController.deleteTask);
-
-module.exports = router;
